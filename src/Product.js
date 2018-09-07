7a4f69e3c58052a6c7d378588b8b2042a6049ea3
Corentin Leruth <tinencor@gmail.com>
2018-09-07 17:40:08 +0200

    updates

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Button, Switch } from 'antd';
 import './Product.css';
 
 const Product = props => {
-  return <div className="btn-blue">my Product</div>;
+  return (
+    <div>
+      <div className="btn-blue">my Product</div>
+      <Button>Coucou</Button>
+      <Switch defaultChecked={true} />
+    </div>
+  );
 };
 
 export default Product;
