460fa1a2fba1ae5efc6c69a9c64a597b0ada9676
Corentin Leruth <tinencor@gmail.com>
2018-09-11 21:50:22 +0200

    clean up

--- app/src/SharedComp.js
+++ app/src/SharedComp.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const SharedComp = props => {
   return (
@@ -7,10 +6,6 @@ const SharedComp = props => {
       Shared
     </div>
   );
-};
-
-SharedComp.propTypes = {
-
 };
 
 export default SharedComp;
